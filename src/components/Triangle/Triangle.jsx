5534176f23e394676d281076a2d5b603ffd93ec8
Amram1997 <amramananyan9@gmail.com>
2022-08-18 18:46:58 +0400

    feat:add triangle functions

---
+++ src/components/Triangle/Triangle.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react'
+import triangle from '../../helpers/createTriangle';
+import "./style.css";
+function Triangle() {
+    const [value, setValue] = useState("");
+    const [state, setState] = useState([]);
+ 
+    useEffect(()=>{
+        setState(triangle(value))
+    },[value,setValue])
+  
+    function reverseTriangle() {
+      setState(triangle(value).reverse())
+    }
+    
+    function handleInput(e) {
+        if (typeof +e.target.value === "number") {
+             setValue(e.target.value)
+        } 
+       
+    }
+  return (
+      <div className='page'>
+          <input type="text" className='add-number' value={value} onChange={(evt)=>handleInput(evt)} />
+          <div className='triangle-area'>
+              <div>
+              {
+                  state.map((item,index) => {
+                      return <span key={index}>{ item}</span>
+                 }) 
+              }
+              </div>
+          </div>
+          <button className='reverse-btn' onClick={reverseTriangle}>Reverse</button>
+    </div>
+  )
+}
+
+export default Triangle
---
+++ src/helpers/createTriangle.js
@@ -0,0 +1,22 @@
+function triangle(n) {
+    if (n) {
+        let newArr = []; 
+        for(let i=1;i<=n;i++){
+                for(let j=0;j<=n;j++){
+                            if (i+j>n) {
+                                newArr.push(<span>&nbsp;&nbsp;&nbsp;&nbsp;</span>);
+                               
+                }else{
+                newArr.push(<span>*&nbsp;&nbsp;</span>)
+            }
+                }   
+            newArr.push(<br/>)
+        }
+        return newArr
+    } else {
+        return []
+    }
+   
+};
+
+export default triangle
