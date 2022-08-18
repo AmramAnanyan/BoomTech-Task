5d26f9bc844d4f4e188531da30e67c9785853728
Amram1997 <amramananyan9@gmail.com>
2022-08-18 19:14:06 +0400

    fix: change condition in creatTriangle function

--- src/helpers/createTriangle.js
+++ src/helpers/createTriangle.js
@@ -1,7 +1,7 @@
 function triangle(n) {
     if (n) {
         let newArr = []; 
-        for(let i=1;i<=n;i++){
+        for(let i=0;i<=n;i++){
                 for(let j=0;j<=n;j++){
                             if (j<=i) {
                                 newArr.push(<span>&nbsp;&nbsp;&nbsp;&nbsp;</span>);
